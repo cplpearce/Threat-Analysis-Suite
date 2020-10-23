6db7a5d86e27b09e35e4c066f45f44dd8402dad1
Clinton Pearce <clint.pearce@rocketmail.com>
2020-10-22 23:43:28 -0400

    Add datatable viewer, css fixes to centering

--- public/js/viewReports.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#form-filter").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#reports-table tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
